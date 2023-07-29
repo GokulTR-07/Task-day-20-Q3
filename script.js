6623172151e9f4b5c4c6e8356468fa58ea6d2bcd
GokulTR-07 <137423253+GokulTR-07@users.noreply.github.com>
2023-07-29 09:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-var res = fetch("http://api.citybik.es/v2/networks")
+var res = fetch("https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand")
 .then((data)=>data.json()).then((data1)=>{
-  for(let i in data1){
-    //console.log(data1[i]);
-    for(let j in data1[i]){
-      console.log(data1[i][j])
-      var div = document.createElement("div")
+  for(var i=0; i<data1.length; i++){
+    console.log(data1[1]);
+    
+    var div = document.createElement("div")
       div.setAttribute("class","box");
       div.innerHTML = `
       <div class="flex-container">
       <div class="row">
         <div class="col col-sm-12">
         <div class="card text-white bg-dark mb-3">
-      <div class="card-header"><h5 class="d-inline">Company: </h5>${data1[i][j].company}</div>
+      <div class="card-header"><h5 class="d-inline">Title: ${data1[i].title.rendered}</div>
       <div class="card-body">
-        <h5 class="card-title">Name: ${data1[i][j].name}</h5>
-        <p class="card-text">City: ${data1[i][j].location.city}</p>
-        <p class="card-text">Country: ${data1[i][j].location.country}</p>
+        <h5 class="card-title">Content: ${data1[i].content.rendered}</h5>
       </div>
     </div>
         </div>
@@ -24,7 +21,5 @@ var res = fetch("http://api.citybik.es/v2/networks")
       `
 
     document.body.append(div);
-    }
   }
 })
-
